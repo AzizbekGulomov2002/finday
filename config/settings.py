321238ee1c826a3c7ef659817aa9d92b05fab11e
Macbook Air M2 <macbookairm2@Air-Macbook.localdomain>
2025-07-10 15:07:43 +0500

    [ADDED] expenses and incomes

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6v2@9msh-!i80_)s6md1j7&pi18d@&bm%!87*lo$n5@1&w++$9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
